5a6ed21fa1a2ab18dad62eecaa8ad3a3afcbaf06
Chaoyi Wu <chaoyiwu20@gmail.com>
2025-03-24 12:34:29 -0400

    updated about ui & rebranded interests nested route

--- src/components/views/About.vue
+++ src/components/views/About.vue
@@ -4,37 +4,40 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div>
-    <h1>About Page</h1>
-  </div>
-  <div class="aboutPerson">
-  <div class="aboutPersonInfo">
-  <h2> Jacklyn Becker </h2>
-  <h3 class="infoSection"> This is where I write a section about myself. </h3>
-  <div class="githubLink">
-    <a href="https://github.com/JacklynBecker">
-      <button class="githubLinkButton">Github Link</button>
-    </a>
-  </div>
-  <RouterLink to="/about/Jacklyn/interests" class="tab">Interests</RouterLink>
-  </div>
-  <img alt="Jacky logo" class="aboutImage" src="../../assets/jacky.png" width="250" height="250" />
-  
-  </div>
-  <div class="aboutPerson">
-  <div class="aboutPersonInfo">
-  <h2> Chaoyi Wu </h2>
-  <h3 class="infoSection"> This is where I write a section about myself. </h3>
-  <div class="githubLink">
-    <a href="https://github.com/chaoyiuwu">
-      <button class="githubLinkButton">Github Link</button>
-    </a>
-  </div>
-  <RouterLink to="/about/Chaoyi/interests" class="tab">Interests</RouterLink>
-  </div>
-  <img alt="Chaoyi logo" class="aboutImage" src="../../assets/jacky.png" width="250" height="250" />
-  <router-view/>
-  </div>
+    <div class='flex flex-col items-center px-10'>
+        <div class="aboutPerson">
+        <div class="aboutPersonInfo">
+        <p class="text-lg"> Jacklyn Becker </p>
+        <h3 class="infoSection"> This is where I write a section about myself. </h3>
+        <div class="flex flex-row justify-around py-2">
+            <a href="https://github.com/JacklynBecker">
+            Github Link
+            </a>
+            <RouterLink to="/about/Jacklyn/moreabout" class="">Discover More</RouterLink>
+        </div>
+
+        </div>
+        <img alt="Jacky logo" 
+        class="aboutImage rounded-full w-50 h-auto" 
+        src="../../assets/jacky.png"/>
+        
+        </div>
+        <div class="aboutPerson">
+        <div class="aboutPersonInfo">
+        <p class="text-lg"> Chaoyi Wu </p>
+        <h3 class="infoSection">Hey there! My name is Chaoyi and my entire personality consists of reading books, playing video games and petting cats. (I have two and their names are Miwa and Lola!)</h3>
+        <div class="flex flex-row justify-around py-2">
+            <a href="https://github.com/chaoyiuwu">Github Link</a>
+            <RouterLink to="/about/Chaoyi/moreabout" class="">Discover More</RouterLink>
+        </div>
+
+        </div>
+        <img alt="Chaoyi logo"
+        class="aboutImage rounded-full w-50 h-auto"
+        src="../../assets/miwalola.png" />
+        </div>
+        <router-view/>
+    </div>
 </template>
 
 <style scoped>
@@ -47,41 +50,30 @@ h1{
   flex-direction:row;
   margin-top:10%;
   margin: auto;
-  width: 50%;
-  border: 3px solid green;
   padding: 10px;
   margin-bottom: 5%;
-  
 }
 .aboutImage{
-  
-  border: 5px solid #555;
+  border: 2px solid #555;
   border-radius: 50%;
-
-
 }
 .aboutPersonInfo{
   display: flex;
   flex-direction:column;
   padding-right:10%;
   justify-content: center;
-
 }
 
 .githubLink{
   margin-top:10%;
-
 }
 
-.githubLinkButton{
-
+.LinkButton{
   padding-top:5%;
   padding-left:15%;
   padding-right:15%;
   padding-bottom:5%;
-  background-color:#ADD8E6;
   border-radius: 5%;
-  border-color:#ADD8E6;
 }
 
 .infoSection{
--- src/router.js
+++ src/router.js
@@ -3,14 +3,14 @@ import { createWebHistory, createRouter } from 'vue-router'
 import Home from './components/views/Home.vue'
 import About from './components/views/About.vue'
 import Posts from './components/views/Posts.vue'
-import Interests from './components/views/Interests.vue'
+import MoreAbout from './components/views/MoreAbout.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/about', 
     component: About, 
     children: [{
-      path: ':name/interests', component: Interests
+      path: ':name/moreabout', component: MoreAbout
     }] },
   { path: '/posts', component: Posts }
 ]
